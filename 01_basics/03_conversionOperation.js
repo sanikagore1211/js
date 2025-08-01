59f39451a82fc7891f5e51d4056f94ca41451eb8
sanikagore1211 <sanikavgore2020@gmail.com>
2025-08-01 17:27:16 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+
+let score = "33"      //it give value as 33 after conversion 
+let score1 = "33abc"   //after converting this type of string it gives value as 'NaN' that is not a number 
+let score2 = null
+let score3 = undefined
+let score4 = true
+let score5 = "sanika"
+
+console.log(typeof score);
+console.log(typeof(score1));
+
+console.log(typeof(score2));
+console.log(typeof(score3));
+console.log(typeof(score4));
+console.log(typeof(score5));
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+let valueInNumber5 = Number(score5)
+
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1);
+console.log(typeof valueInNumber2);
+console.log(typeof valueInNumber3);
+console.log(typeof valueInNumber4);
+console.log(typeof valueInNumber5);
+
+console.log(valueInNumber)
+console.log(valueInNumber1)
+console.log(valueInNumber2)
+console.log(valueInNumber3)
+console.log(valueInNumber4)
+console.log(valueInNumber5)
+
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1      false ==> 0
+
+
+let isLoggedIn = 1
+let isLoggedIn1 = 0
+let isLoggedIn2 = ""
+let isLoggedIn3 = "sanika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+let booleanIsLoggedIn3 = Boolean(isLoggedIn3)
+
+console.log(booleanIsLoggedIn)
+console.log(booleanIsLoggedIn1)
+console.log(booleanIsLoggedIn2)
+console.log(booleanIsLoggedIn3)
+
+// 0 ==> false   1 ==> true
+// "" ==> false
+// "sanika" ==> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
