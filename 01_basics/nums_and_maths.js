9b8bf3e4c8329c200f53eaee8fca6bfdc45954c9
sanikagore1211 <sanikavgore2020@gmail.com>
2025-08-02 16:53:30 +0000

    math and num

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.8652
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-2));
+console.log(Math.abs(3));
+console.log(Math.round(5.6));
+console.log(Math.ceil(5.6));
+console.log(Math.floor(5.6));
+console.log(Math.min(5, 2, 3, 4));
+console.log(Math.max(5, 2, 3, 4));
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
